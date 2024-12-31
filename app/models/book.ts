e34c2ed1df7a40534154e5b5680591111c46cad5
OMKAR AGRAWAL <10913160+Omkaragrawal@users.noreply.github.com>
2024-12-31 01:27:01 +0000

    feat: 🎸 add tests

--- app/models/book.ts
+++ app/models/book.ts
@@ -1,6 +1,7 @@
 import { column } from '@adonisjs/lucid/orm';
 import BaseStringIdModel from '#models/BaseStringIdModel';
 import { BookGenreEnum } from '#enums/book';
+import { DateTime } from 'luxon';
 
 export default class Book extends BaseStringIdModel {
   public static override table = 'tbl_books';
@@ -22,4 +23,15 @@ export default class Book extends BaseStringIdModel {
 
   @column()
   public checkedOut: boolean;
+
+  @column.dateTime({
+    autoCreate: true,
+    serialize: (value: DateTime | null) => {
+      return value ? value.toFormat('dd-MM-yyyy HH:mm:ss ZZ') : value;
+    },
+  })
+  public override createdAt: DateTime;
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true, serializeAs: null })
+  public override updatedAt: DateTime;
 }
