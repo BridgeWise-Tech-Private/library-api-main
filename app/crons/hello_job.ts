80a6138c2ff412482c896161111fae575c30c1a3
OMKAR AGRAWAL <10913160+Omkaragrawal@users.noreply.github.com>
2025-01-17 07:52:24 +0000

    feat: 🎸 add delete job cron

--- app/crons/hello_job.ts
+++
@@ -1,7 +0,0 @@
-import { BaseJob } from '#types/cronjob';
-
-export default class HelloJob extends BaseJob {
-    public run(): void {
-        console.log('Hello!');
-    }
-}
--- start/scheduler.ts
+++ start/scheduler.ts
@@ -1,16 +1,17 @@
 // start/scheduler.ts
 import SchedulerService from '#services/scheduler_service';
-// import HelloJob from '#crons/hello_job';
+import DeleteEntriesJob from '#crons/delete_entries_job';
+
 
 // Create an instance of the scheduler service on server startup
 const scheduler = new SchedulerService();
 
 // Add all jobs which should be run while the server is up
-// scheduler.addJob({
-//     key: 'hello-job',
-//     cronExpression: '* * * * *',
-//     job: new HelloJob(),
-// });
+scheduler.addJob({
+    key: 'delete-old-entries',
+    cronExpression: '0 0 * * *',
+    job: new DeleteEntriesJob(),
+});
 
 // Actually start a scheduler for all jobs
 scheduler.scheduleAllJobs();
